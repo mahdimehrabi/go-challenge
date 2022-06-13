04fb1597c89be72470c177cb7f19c3d2169a2e99
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-13 18:00:14 +0430

    Updated db interface

--- app/interfaces/db.go
+++ app/interfaces/db.go
@@ -2,6 +2,28 @@ package interfaces
 
 import "context"
 
+type Model interface {
+	SliceToModel(data []interface{}) error
+}
+
 type DB interface {
-	Query(ctx context.Context, query string, scans []interface{})
+	//for execution of query
+	Exec(
+		ctx context.Context,
+		query string,
+		parameters []interface{}) error
+
+	//get signle row
+	QueryRow(
+		ctx context.Context,
+		query string,
+		parameters []interface{},
+		scans []interface{}) error
+
+	//get multiple rows
+	Query(ctx context.Context,
+		query string,
+		parameters []interface{},
+		model *Model,
+	) error
 }
