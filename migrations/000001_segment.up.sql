842f1ea1f94a6b23ee625a1df29650e68b07ab8e
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-14 01:36:20 +0430

    Fixed migration sql

--- migrations/000001_segment.up.sql
+++ migrations/000001_segment.up.sql
@@ -4,5 +4,5 @@ For the purpose of this project I dont create segment table and users table is e
 CREATE TABLE "users" (
     ID      VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
     segment VARCHAR(50) UNIQUE NOT NULL,
-    expired_segment  TIMESTAMP,
+    expired_segment  TIMESTAMP
 )
