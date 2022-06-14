33e08a0faf8490366cedefec1db07023766a2b6d
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-14 17:03:30 +0430

    Refactor

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -8,8 +8,10 @@ import (
 	"challange/app/services"
 	"context"
 	"go.uber.org/fx"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -43,25 +45,29 @@ func Bootstrap(
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
 		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
-	lifecycle.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			// start the server
-			logger.LG.Println("Starting server on port " + port)
 
-			err := s.ListenAndServe()
-			if err != nil {
-				logger.LG.Printf("Error starting server: %s\n", err)
-				return err
-			}
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			err := db.Conn.Close(ctx)
-			if err != nil {
-				return err
-			}
-			return s.Shutdown(ctx)
-		},
-	})
+	// start the server
+	go func() {
+		logger.LG.Println("Starting server on port " + port)
+
+		err := s.ListenAndServe()
+		if err != nil {
+			logger.LG.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 
 }
