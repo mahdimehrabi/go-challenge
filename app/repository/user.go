a645db78eca3e565062371a27ab2176fa3e70f24
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-14 10:59:36 +0430

    Added user repo save method + test

---
+++ app/repository/user.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"challange/app/interfaces"
+	"challange/app/models"
+	"context"
+	"time"
+)
+
+type UserRepository struct {
+	logger   interfaces.Logger
+	db       interfaces.DB
+	memoryDb interfaces.MemoryDB
+}
+
+func NewUserRepository(
+	logger interfaces.Logger,
+	db interfaces.DB,
+	memoryDB interfaces.MemoryDB) UserRepository {
+	return UserRepository{
+		db:       db,
+		logger:   logger,
+		memoryDb: memoryDB,
+	}
+}
+
+func (us *UserRepository) Save(userID string, segment string, ExpiredSegment time.Time) error {
+	ctx := context.TODO()
+	parameters := []interface{}{
+		userID, segment, ExpiredSegment,
+	}
+	_, err := us.db.Exec(
+		ctx,
+		"INSERT INTO users (ID,segment,expired_segment) values($1,$2,$3)",
+		parameters)
+	return err
+}
+
+func (us *UserRepository) List() (users []*models.User, err error) {
+	ctx := context.TODO()
+
+	values, err := us.db.Query(
+		ctx,
+		"SELECT * FROM users",
+		[]interface{}{})
+	if err != nil {
+		return
+	}
+	for _, v := range values {
+		users = append(users, &models.User{
+			ID:            v[0].(string),
+			Segment:       v[1].(string),
+			ExpireSegment: v[2].(time.Time),
+		})
+	}
+	return
+}
